a1c18e15723a7cb453534df8267e13d2431d0cc3
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-03-17 11:26:08 -0300

    Melhorando o teleop
    Iniciando a melhora teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Modo_Teleoperado.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp
+public class Modo_Teleoperado extends OpMode {
+
+    //Variáveis
+    double sen, cos, theta, power, max;
+    double leftf,leftb, rightf, rightb;
+
+    @Override
+    public void init(){
+
+    }
+
+    public void loop(){
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/LConstants.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.constants;
+
+import com.pedropathing.localization.*;
+import com.pedropathing.localization.constants.*;
+
+public class LConstants {
+    static {
+        ThreeWheelConstants.forwardTicksToInches = .001989436789;
+        ThreeWheelConstants.strafeTicksToInches = .001989436789;
+        ThreeWheelConstants.turnTicksToInches = .001989436789;
+        ThreeWheelConstants.leftY = 1;
+        ThreeWheelConstants.rightY = -1;
+        ThreeWheelConstants.strafeX = -2.5;
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftf";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightb";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightf";
+        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+    }
+}
+
+
+
+
