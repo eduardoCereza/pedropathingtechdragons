f9d1c572e1b23e523779b7879ed23a06c7801a0a
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-03-26 16:07:42 -0300

    editando importações

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/ExampleRobotCentricTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/examples/ExampleRobotCentricTeleop.java
@@ -8,8 +8,9 @@ import com.pedropathing.util.Constants;
 import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import pedroPathing.constants.FConstants;
-import pedroPathing.constants.LConstants;
+import org.firstinspires.ftc.teamcode.constants.FConstants;
+import org.firstinspires.ftc.teamcode.constants.LConstants;
+
 
 /**
  * This is an example teleop that showcases movement and robot-centric driving.
