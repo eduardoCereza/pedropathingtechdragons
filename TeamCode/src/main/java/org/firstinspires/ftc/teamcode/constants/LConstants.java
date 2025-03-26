0c36c8e7fd345977b1a9556e79c5e8a2eac6def7
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-03-26 15:25:49 -0300

    Adicionando classes no Pedro Pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/LConstants.java
@@ -11,9 +11,9 @@ public class LConstants {
         ThreeWheelConstants.leftY = 1;
         ThreeWheelConstants.rightY = -1;
         ThreeWheelConstants.strafeX = -2.5;
-        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftf";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightb";
-        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightf";
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
         ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
         ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
         ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
