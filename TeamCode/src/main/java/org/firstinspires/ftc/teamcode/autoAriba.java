718711485cd19d5edf8500f99727920f9396b6a2
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-04-17 13:36:42 -0300

    add git henry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoAriba.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoAriba.java
@@ -17,24 +17,116 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
 @Autonomous(name = "Ariba mexico", group = "Examples")
 public class autoAriba extends OpMode {
 
-    private Servo servo1, servo2, garra;
+    public void clipPos(){
+        leftS.setPosition(0.0);
+        rightS.setPosition(1.0);
+        clippos = 1;
+        pickpos = 0;
+        specimenpickpos = 0;
+    }
+    public void pickPos(){
+        leftS.setPosition(1.0);
+        rightS.setPosition(0.0);
+        clippos = 0;
+        pickpos = 1;
+        specimenpickpos = 0;
 
-    private DcMotor slide, armMotorL, armMotorR;//servo da garra/ponta
+    }
+    public void specimenPickpos(){
+        leftS.setPosition(0.6);
+        rightS.setPosition(0.4);
+        clippos = 0;
+        pickpos= 0;
+        specimenpickpos = 0;
+    }
+    public void closed(){
+        garra.setPosition(0.0);
+        isopen = 0;
+    }
+    public void open(){
+        garra.setPosition(0.6);
+        isopen = 1;
+    }
+    public void subir(int target){
 
+        while (Left.getCurrentPosition() <= -target && Right.getCurrentPosition() <= target){
+            Left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            Right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            Left.setTargetPosition(-target);
+            Right.setTargetPosition(target);
+
+            Left.setPower(-1);
+            Right.setPower(1);}
+    }
+    public void descer(int target){
+
+        while (Left.getCurrentPosition() >= -target && Right.getCurrentPosition() >= target){
+            Left.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            Right.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            Left.setTargetPosition(target);
+            Right.setTargetPosition(-target);
+
+            Left.setPower(1);
+            Right.setPower(-1);
+        }
+    }
+    public void hold(){
+
+        PIDFController controller;
+
+        double minPower = 0.2;
+        double maxPower = 0.5;
+        controller = new PIDFController(10, 3, 4, 12);
+        controller.setInputRange(-4000, 4000);
+        controller.setOutputRange(minPower, maxPower);
+
+        double powerM = maxPower + controller.getComputedOutput(Left.getCurrentPosition());
+        double powerM1 = maxPower + controller.getComputedOutput(Right.getCurrentPosition());
+
+        Left.setTargetPosition(Left.getCurrentPosition());
+        Right.setTargetPosition(Right.getCurrentPosition());
+
+        Left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        Left.setPower(powerM);
+        Right.setPower(powerM1);
+
+    }
+    public void extender( int target){
+
+        while (slide.getCurrentPosition() >= target){
+            slide.setTargetPosition(-target);
+            slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            slide.setPower(-1.0);
+        }
+        slide.setPower(0.0);
+    }
+    public void recuar(int target){
+
+        while(slide.getCurrentPosition() >= target){
+            slide.setTargetPosition(target);
+            slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            slide.setPower(1.0);
+        }
+        slide.setPower(0.0);
+    }
+    int isopen;
+    int  specimenpickpos, clippos, pickpos;
+    private DcMotorEx slide, Left, Right;
+    private Servo garra, servo1, servo2; //servo da garra/ponta
+    private Servo leftS, rightS;
     private Follower follower; //sla tbm
-
-    private Timer pathTimer, actionTimer, opmodeTimer; //sla ja veio no código
-
+    private Timer pathTimer, opmodeTimer; //sla ja veio no código
     private int pathState; //variável de controle das trajetórias e ações
-
-
     // y = lados (se for maior vai para a direita)
     // x = frente e tras (se for maior vai para frente)
     private final Pose startPose = new Pose(0, 71, Math.toRadians(180)); //posição inicial do robô
@@ -50,7 +142,6 @@ public class autoAriba extends OpMode {
     private final Pose move9 = new Pose(50, -30, Math.toRadians(180)); //foi para a direita na frente do terceiro sample
     private final Pose move10 = new Pose(5, -30, Math.toRadians(180)); //empurrou o terceiro sample para a area do jogador humano
     private final Pose move11 = new Pose(30, -30, Math.toRadians(180)); // voltou para frente
-
     private PathChain traj1; //conjunto de trajetórias
     public void buildPaths() {
 
@@ -130,7 +221,6 @@ public class autoAriba extends OpMode {
     }
 
     //controle das trajetórias
-
     public void setPathState(int pState) {
         pathState = pState;
         pathTimer.resetTimer();
@@ -143,7 +233,34 @@ public class autoAriba extends OpMode {
         Pose pose = follower.getPose();
         if (pose.getX() == ClipPose.getX() && pose.getY() == ClipPose.getY()){
 
+            open();
+        }
 
+        if (follower.isBusy() && slide.getPower() < 0.3){
+            int currentPosition = slide.getCurrentPosition();
+
+            slide.setTargetPosition(currentPosition); // Define a posição atual como alvo
+            slide.setMode(DcMotor.RunMode.RUN_TO_POSITION); // Mantém o motor na posição
+            slide.setPower(0.1); // Aplica uma pequena potência para segurar a posição
+
+        }
+        if (follower.isBusy() && Left.getPower() < 0.3 && Right.getPower() < 0.3){
+            hold();
+        }
+        if (isopen == 0){
+            garra.setPosition(0.0);
+        }
+        if (clippos == 1){
+            leftS.setPosition(0.0);
+            rightS.setPosition(1.0);
+        }
+        if (pickpos == 1){
+            leftS.setPosition(1.0);
+            rightS.setPosition(0.0);
+        }
+        if (specimenpickpos == 1){
+            leftS.setPosition(0);
+            rightS.setPosition(0);
         }
 
         follower.update();
@@ -159,22 +276,18 @@ public class autoAriba extends OpMode {
     //se precisar fazer alguma ação no init tem que por aq
     @Override
     public void init() {
+
+        isopen = 0;
+
         slide = hardwareMap.get(DcMotorEx.class, "gobilda");
-        slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftS = hardwareMap.get(Servo.class, "lservo");
+        rightS = hardwareMap.get(Servo.class, "rservo");
+        garra = hardwareMap.get(Servo.class, "garra");
+        Left = hardwareMap.get(DcMotorEx.class, "Left");
+        Right = hardwareMap.get(DcMotorEx.class, "Right");
 
         servo1 = hardwareMap.get(Servo.class, "servo1");
         servo2 = hardwareMap.get(Servo.class, "servo2");
-        garra = hardwareMap.get(Servo.class, "garra");
-
-        armMotorL = hardwareMap.get(DcMotorEx.class, "armmotorleft");
-        armMotorR = hardwareMap.get(DcMotorEx.class, "armmotorright");
-        armMotorL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        armMotorR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        armMotorR.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        armMotorL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armMotorR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         pathTimer = new Timer();
         opmodeTimer = new Timer();
@@ -188,7 +301,9 @@ public class autoAriba extends OpMode {
 
     //só um loop pra quando dar init
     @Override
-    public void init_loop() {}
+    public void init_loop() {
+
+    }
 
     //quando começar ele define a variável de controle como 0 e ja começa as ações
     @Override
