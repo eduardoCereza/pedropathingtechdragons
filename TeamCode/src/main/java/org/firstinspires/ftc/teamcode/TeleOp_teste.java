b18f74ac7e809b163d545647e360e0a7d4d3abfe
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-04-24 17:16:44 -0300

    editando lógica do PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_teste.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_teste.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.constants.FConstants;
 import org.firstinspires.ftc.teamcode.constants.LConstants;
@@ -23,18 +24,30 @@ import org.firstinspires.ftc.teamcode.constants.LConstants;
 @Config
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "TeleOperado teste")
 public class TeleOp_teste extends OpMode {
+
+    double kP = 0.01;
+    double kI = 0.001;
+    double kD = 0.0005;
+
+    double integralL = 0;
+    double integralR = 0;
+    double lastErrorL = 0;
+    double lastErrorR = 0;
+
     private Follower follower;
     DcMotorEx slide, armMotorL, armMotorR;
-    double powerR, powerL;
     Servo servo1, servo2, garra;
     boolean holdingPosition = false, modeBase = false;
     private final Pose startPose = new Pose(0, 0, 0);
-    PController pidL, pidR;
 
-    int estado;
+    int estado, targetPosition;
+
+    long now, lastTime;
 
     @Override
     public void init() {
+        lastTime = System.nanoTime();
+
         Constants.setConstants(FConstants.class, LConstants.class);
         follower = new Follower(hardwareMap, FConstants.class, LConstants.class);
         follower.setStartingPose(startPose);
@@ -120,16 +133,63 @@ public class TeleOp_teste extends OpMode {
     public void armBase() {
         double j = gamepad2.right_stick_y;
 
-        if(j > 0 || j < 0 ){
-            estado = 1;
-        }else if(j == 0){
-            estado = 2;
+        if (j != 0) {
+            estado = 1;  // O joystick está sendo movido
+        } else {
+            estado = 2;  // O joystick está solto, deve segurar a posição
         }
 
-        if(estado == 1){
-            double currentPos
+        // Parâmetros do PID
+
+        long lastTime = System.nanoTime();
+
+        if (estado == 1) {
+            // Controle manual quando o joystick está sendo movido
+            armMotorL.setPower(j);
+            armMotorR.setPower(j);
+
+            // Salva a posição como o "targetPosition" quando o movimento começa
+            targetPosition = (int) ((armMotorL.getCurrentPosition() + armMotorR.getCurrentPosition()) / 2.0);
+            integralL = 0;  // Zera o integral ao iniciar o movimento
+            integralR = 0;
+        } else if (estado == 2) {
+            // Controle PID para segurar a posição quando o joystick está solto
+
+            long now = System.nanoTime();
+            double deltaTime = (now - lastTime) / 1e9;
+            lastTime = now;
+
+            // Posição atual dos motores
+            double currentPosL = armMotorL.getCurrentPosition();
+            double currentPosR = armMotorR.getCurrentPosition();
+
+            // Erro para o motor esquerdo (L)
+            double errorL = targetPosition - currentPosL;
+            integralL += errorL * deltaTime;
+            double derivativeL = (errorL - lastErrorL) / deltaTime;
+            double outputL = kP * errorL + kI * integralL + kD * derivativeL;
+
+            // Erro para o motor direito (R)
+            double errorR = targetPosition - currentPosR;
+            integralR += errorR * deltaTime;
+            double derivativeR = (errorR - lastErrorR) / deltaTime;
+            double outputR = kP * errorR + kI * integralR + kD * derivativeR;
+
+            // Aplica a potência com limites de -0.5 a 0.5
+            armMotorL.setPower(Range.clip(outputL, -0.5, 0.5));
+            armMotorR.setPower(Range.clip(outputR, -0.5, 0.5));
+
+            // Atualiza os erros para a próxima iteração
+            lastErrorL = errorL;
+            lastErrorR = errorR;
         }
+
+        // Atualiza o telemetria para debugar
+        telemetry.addData("Left Pos", armMotorL.getCurrentPosition());
+        telemetry.addData("Right Pos", armMotorR.getCurrentPosition());
+        telemetry.update();
     }
+
 
 
     //Todo: Mover servo
