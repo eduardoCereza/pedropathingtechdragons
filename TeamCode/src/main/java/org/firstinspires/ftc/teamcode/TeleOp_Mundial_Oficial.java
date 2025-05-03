97972b82562478391ce7188d3411ee48dca7697d
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-05-03 18:39:02 -0300

    editando força pra descer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Mundial_Oficial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Mundial_Oficial.java
@@ -157,10 +157,10 @@ public class TeleOp_Mundial_Oficial extends OpMode {
         // Se o joystick for movido para baixo e ainda não atingiu o limite, move o motor
         else if (j < 0) {
             armMotorL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            armMotorL.setPower(-0.1);
+            armMotorL.setPower(-0.2);
 
             armMotorR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            armMotorR.setPower(-0.1);
+            armMotorR.setPower(-0.2);
             modeBase = false; // O motor está se movendo, então não está segurando posição
         }
         // Se o joystick estiver parado e o motor ainda não estiver segurando a posição
