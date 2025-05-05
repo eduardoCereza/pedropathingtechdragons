87a919791d75a706b78eea133e119270c4dc4a15
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-05-05 16:34:37 -0300

    autonomo para testar

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoAribaCesta.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoAribaCesta.java
@@ -149,7 +149,7 @@ public class AutoAribaCesta extends OpMode {
     private int pathState; //variável de controle das trajetórias e ações
     // y = lados (se for maior vai para a direita)
     // x = frente e tras (se for maior vai para frente)
-    private final Pose startPose = new Pose(0, 70, Math.toRadians(180)); //posição inicial do robô
+    private final Pose startPose = new Pose(0, 80, Math.toRadians(180)); //posição inicial do robô
     private final Pose move1 = new Pose(-10, 20, Math.toRadians(180));
     private final Pose move2 = new Pose(-5, 20, Math.toRadians(180));
     private final Pose move3 = new Pose(31.22, 125.5, Math.toRadians(180));
@@ -158,10 +158,17 @@ public class AutoAribaCesta extends OpMode {
     private final Pose move6 = new Pose(93.438, 81.783, Math.toRadians(180));
 
 
-    private PathChain traj1, traj2, traj3, traj4, traj5, traj6, traj7; //conjunto de trajetórias
+    private PathChain traj0, traj1, traj2, traj3, traj4, traj5, traj6, traj7; //conjunto de trajetórias
 
     public void buildPaths() {
 
+        /*
+        traj0 = follower.pathBuilder()
+                //vai até o primeiro specimen amarelo
+                .addPath(new BezierLine(new Point(startPose), new Point(move1)))
+                .setConstantHeadingInterpolation(Math.toRadians(180)).build();
+
+         */
         traj1 = follower.pathBuilder()
                 //vai até o primeiro specimen amarelo
                 .addPath(new BezierLine(new Point(startPose), new Point(move1)))
@@ -199,10 +206,79 @@ public class AutoAribaCesta extends OpMode {
         switch (pathState) {
             //faz a trajetória
             case 0:
-                //inicia a trajetória
-                subir(-650);
                 follower.followPath(traj1, 0.6,true);
                 pathState = 1;
+                break;
+            case 1:
+                extender(-1500);
+                closed();
+                recuar(0);
+                pathState = 2;
+                break;
+            case 2:
+                follower.followPath(traj2, true);
+                pathState = 3;
+                break;
+            case 3:
+                clipPos();
+                subir(-650);
+                extender(-1800);
+                open();
+                specimenPickpos();
+                recuar(0);
+                descer(0);
+                pathState = 4;
+                break;
+            case 4:
+                follower.followPath(traj3, true);
+                pathState = 5;
+                break;
+            case 5:
+                extender(-1500);
+                closed();
+                recuar(0);
+                pathState = 6;
+                break;
+            case 6:
+                follower.followPath(traj4, true);
+                pathState = 7;
+                break;
+            case 7:
+                clipPos();
+                subir(-650);
+                extender(-1800);
+                open();
+                specimenPickpos();
+                recuar(0);
+                descer(0);
+                pathState = 8;
+                break;
+            case 8:
+                follower.followPath(traj5, true);
+                pathState = 8;
+                break;
+            case 9:
+                extender(-1500);
+                closed();
+                recuar(0);
+                pathState = 10;
+                break;
+            case 10:
+                follower.followPath(traj6, true);
+                pathState = 11;
+                break;
+            case 11:
+                clipPos();
+                subir(-650);
+                extender(-1800);
+                open();
+                specimenPickpos();
+                recuar(0);
+                descer(0);
+                pathState = 12;
+                break;
+            case 12:
+                follower.followPath(traj7, true);
                 break;
         }
     }
